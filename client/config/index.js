0fd4ae3b62363aba5d900427141a6f4ef3027138
Tommy Huang <free44458@gmail.com>
2022-01-23 21:36:02 +0800

    change port to "7070" and README add env installation for apple M1

--- client/config/index.js
+++ client/config/index.js
@@ -23,7 +23,7 @@ module.exports = {
     },
     dev: {
         env: require('./dev.env'),
-        port: 7000,
+        port: 7070,
         autoOpenBrowser: true,
         assetsSubDirectory: 'static',
         assetsPublicPath: '/',
